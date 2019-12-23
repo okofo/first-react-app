1b7c734feae753524360d9c3956c8db1cf424b29
R.OKOFO <aidward32@gmail.com>
2019-12-23 21:33:10 +0100

    will get back to this

--- src/App.js
+++ src/App.js
@@ -1,27 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{Component } from  'react';
+import Todos from './components/Todos';
+import Header from './components/Header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
+
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+      todos : [
+        {
+        id: uuid.v4(),
+        title: "Read a book" ,
+        completed:false
+        } ,
+
+        {
+          id: uuid.v4(),
+          title: "go to class" ,
+          completed:false
+          } ,
+
+          {
+            id: uuid.v4(),
+            title: "call  mom" ,
+            completed:false
+            } ,
+
+            {
+              id: uuid.v4(),
+              title: "Play a game" ,
+              completed:false
+              } 
+      ]
+
+     };
+
+  }
+
+  markComplete = (id)=> {
+       this.setState({todos: this.state.todos.map (todo =>{
+         if(todo.id ===id){
+           todo.completed =!todo.completed
+         }
+         return todo;
+       })});
+  }
+
+  delTodo = (id)=> {
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)] }
+    );
+  }
+
+addTodo= (title) => {
+  const newTodo ={
+    id: uuid.v4() ,
+    title ,
+    completed: false
+  }
+  this.setState({todos: [...this.state.todos ,newTodo]});
+}
+
+
+
+//function App() {
+  render(){
+    console.log(this.state.todos)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTodo addTodo={this.addTodo}/>
+      <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}/>
+    </div>
+   
     </div>
   );
 }
+}
+
 
 export default App;
